462699675ba20b879f0629d97992762141c2e5b2
Olga Latfulina <latfulinaolga601@gmail.com>
2022-11-21 15:50:31 +0300

    Создать форму для добавления статьи на сайте, подключенную к базе данных

--- basketball/main/urls.py
+++ basketball/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
   path('', views.index, name='home'),
-  path('about', views.about, name= 'timetable')
-  ]
+  path('about', views.about, name= 'timetable'),
+  path('cont/', views.cont, name='cont')
+]
